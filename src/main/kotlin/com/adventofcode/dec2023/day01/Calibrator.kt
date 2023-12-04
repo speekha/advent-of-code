bfe0dd393ca1dbbde937c4d7229364c73d104ab7
David Blanc <david.e.blanc@gmail.com>
2023-12-04 17:42:04 +0100

    Day 01-2023 Clean duplication

--- src/main/kotlin/com/adventofcode/dec2023/day01/Calibrator.kt
+++ src/main/kotlin/com/adventofcode/dec2023/day01/Calibrator.kt
@@ -13,51 +13,29 @@ class Calibrator {
     }
 
     private fun computeValue(input: String): Int {
-        var digit1 = -1
-        var digit2 = -1
-        var i = 0
-        while (i < input.length) {
-            if (input[i] in '0'..'9') {
-                digit1 = input[i].digitToInt()
-                break
-            } else {
-                val digit = digitStrings.firstOrNull { input.drop(i).startsWith(it) }
-                if (digit != null) {
-                    digit1 = digitStrings.indexOf(digit)
-                    break
-                } else {
-                    i++
-                }
-            }
-        }
-        i = input.length - 1
-        while (i >= 0) {
-            if (input[i] in '0'..'9') {
-                digit2 = input[i].digitToInt()
-                break
-            } else {
-                val digit = digitStrings.firstOrNull { input.drop(i).startsWith(it) }
-                if (digit != null) {
-                    digit2 = digitStrings.indexOf(digit)
-                    break
-                } else {
-                    i--
-                }
-            }
-        }
-
+        val digit1 = findFirstDigit(input, 0, 1)
+        val digit2 = findFirstDigit(input, input.length - 1, -1)
         return digit1 * 10 + digit2
     }
 
-    private fun mapDigits(inputValue: String): String {
-        var mapped = inputValue
-        while (digitStrings.any { mapped.indexOf(it) != -1 }) {
-            val digit = digitStrings.map { string -> mapped.indexOf(string) to string }.filter { it.first != -1 }
-                .minBy { it.first }.second
-            mapped = mapped.replace(digit, digitStrings.indexOf(digit).toString())
+    private fun findFirstDigit(input: String, start: Int, increment: Int): Int {
+        var digit = -1
+        var i = start
+        while (i in input.indices) {
+            if (input[i] in '0'..'9') {
+                digit = input[i].digitToInt()
+                break
+            } else {
+                val j = digitStrings.firstOrNull { input.drop(i).startsWith(it) }
+                if (j != null) {
+                    digit = digitStrings.indexOf(j)
+                    break
+                } else {
+                    i += increment
+                }
+            }
         }
-
-        return mapped
+        return digit
     }
 
 
